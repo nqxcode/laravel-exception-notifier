29e961206b7cae9ca2338b5513ab0211fdf5b10d
Andrey Pokoev <nc101ux@gmail.com>
2020-11-11 01:36:51 +0300

    Fixed config.

--- src/config/laravel-exception-notifier.php
+++ src/config/laravel-exception-notifier.php
@@ -4,11 +4,11 @@ return [
     'routes' => [
         [
             'channel' => 'mail',
-            'route' => 'example@gmail.com',
+            'route' => env('EXCEPTION_NOTIFIER_MAIL', 'example@gmail.com'),
         ],
         [
             'channel' => 'telegram',
-            'route' => '423460627'
+            'route' => env('EXCEPTION_NOTIFIER_TELEGRAM', '1234567890')
         ]
     ],
     'subject' => 'Исключение на сайте '.request()->root(),
